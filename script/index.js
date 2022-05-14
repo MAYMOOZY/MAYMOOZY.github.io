fd51499862d426f5bdcd17350360e021be776253
maymoozy <69375202+zhangx-x@users.noreply.github.com>
2022-05-14 22:35:32 +0800

    version 1.0.0

--- script/index.js
+++ script/index.js
@@ -1,55 +1,150 @@
+let body = null;
+let container = null;
+let contentArea = null;
+var naviHeight = 0;
+
 window.onload = function () {
-    imgLocation('container', 'box')
+    body = document.getElementsByTagName('Body')[0];
+    container = document.getElementById('container');
+    contentArea = document.getElementById('contentArea');
+    naviHeight = document.getElementById('navi').offsetHeight;
+    loadImg();
+}
+window.onresize = function () {
+    imgLocation('contentArea', 'imgCon');
+}
+function loadImg() {
+    for (let i = 0; i < imgs.length; i++) {
+        // <div class="imgCon">
+        //     <img src="./resource/image/IMG_0275.JPG" alt="">
+        //         <span>desc</span>
+        //         <span>price</span>
+        // </div>
+        let div = document.createElement('div');
+        let img = document.createElement('img');
+        let desc = document.createElement('span');
+        div.appendChild(img);
+        div.appendChild(desc);
+        document.getElementById('contentArea').appendChild(div);
+
+        div.setAttribute('class', 'imgCon');
+        div.setAttribute('onclick', 'checkImgOn(this)');
+        div.style.float = 'left';
+        div.style.padding = '5px';
+        div.style.width = '200px';
+        div.style.border = '1px solid #000';
+        div.style.boxShadow = '0 0 5px #ccc';
+        div.style.borderRadius = '5px';
+        div.style.zIndex = 1;
+        div.style.display = 'flex';
+        div.style.flexDirection = 'column';
+        div.style.alignItems = 'center';
+        div.style.justifyContent = 'center';
+
+        img.setAttribute('src', imgs[i].url);
+        img.style.width = '100%';
+        img.style.zIndex = 1;
+        let name = imgs[i].url.split('/');
+        name = name[name.length - 1].split('.')[0];
+        img.setAttribute('alt', name);
+
+        // desc.innerText = imgs[i].desc;
+        desc.innerText = name;
+        if (i === imgs.length - 1) {
+            img.onload = function () {
+                imgLocation('contentArea', 'imgCon');
+            }
+        }
+    }
 }
 
 // 获取到当前有多少张图片要摆放
 function imgLocation(parent, content) {
     // 将containerd下所有的内容全部取出
-    var cparent = document.getElementById(parent)  //获取container盒子的标签
-    var ccontent = getChildElemnt(cparent, content)//图片时放在container盒子里的box盒子里的，因此我们还需要定义一个函数getChildElemnt（）获取出box里的图片
+    var eParent = document.getElementById(parent);
+    var eContents = getImgCons(eParent, content);
+    
 
-    var imgWidth = ccontent[0].offsetWidth//获取css中我们给每张图片设置的固定宽度
-    var num = Math.floor(document.documentElement.clientWidth / imgWidth) //获取浏览器body的宽度计算最多能放几张我们的图片
-    cparent.style.cssText = `width: ${imgWidth * num} px`
+    var imgWidth = eContents[0].offsetWidth;
+    var imgNumPerRow = Math.floor(eParent.offsetWidth / imgWidth);
+    console.log(imgNumPerRow);
+    eParent.style.width = imgWidth * imgNumPerRow;
 
-    //摆放图片
-    var BoxHeightArr = []
-    for (var i = 0; i < ccontent.length; i++) {
-        if (i < num) {  //我们先将第一行摆满
-            BoxHeightArr[i] = ccontent[i].offsetHeight //这里我们通过BoxHeightArr[]数组存放每列的高度
-        } else { //剩下的图片我们依次次优先选择摆在高度最低的一列后面
-            var minHeight = Math.min.apply(null, BoxHeightArr) //通过将Math.min()中求最小值的方法应用到数组中，求出高度最低的列
-            var minIndex = getMinHeightLocation(BoxHeightArr, minHeight) //确定了高度最低的列后我们就差求出列的位置了,我们通过编写一个函数实现
+
+    let heightPerCol = [];
+
+    //放置图片并计算列高度
+    let offsetLeft = 0;
+    for (let i = 0; i < eContents.length; i++) {
+        //填充第一行
+        if (i < imgNumPerRow) {
+            eContents[i].style.position = 'absolute';
+            eContents[i].style.top = naviHeight + 'px';
+            eContents[i].style.left = offsetLeft + 'px';
+            offsetLeft += eContents[i].offsetWidth;
+            heightPerCol[i] = eContents[i].offsetHeight + naviHeight;
+        }
+        // 将其他图片放置在高度最低的列后面
+        else {
+            let minHeight = Math.min(...heightPerCol)
+
+            let minIndex = getMinHeightColIndex(heightPerCol, minHeight)
 
             //最后将我们的图片相对于container盒子进行定位放在每一列下就可以啦
-            ccontent[i].style.position = 'absolute'
-            ccontent[i].style.top = minHeight + 'px'
-            ccontent[i].style.left = ccontent[minIndex].offsetLeft + 'px'
+            eContents[i].style.position = 'absolute';
+            eContents[i].style.top = minHeight + 'px';
+            eContents[i].style.left = eContents[minIndex].offsetLeft + 'px';
             //最后不忘记跟新每一列的高度哦
-            BoxHeightArr[minIndex] = BoxHeightArr[minIndex] + ccontent[i].offsetHeight
-
+            heightPerCol[minIndex] += eContents[i].offsetHeight;
         }
     }
+    eParent.offsetHeight = Math.max(...heightPerCol) + "px";
 }
 
 
-function getChildElemnt(parent, content) {
-    const contentArr = []
-    const allContent = parent.getElementsByTagName('*')//通过内置函数getElementsByTagName()将container中的所有元素取出来
-    // console.log(allContent);
-    for (var i = 0; i < allContent.length; i++) {//但是container中所有的元素中我们只需要的是所有的img，为此我们写个for循环将所用img筛选出来存放在一个数组中
+function getImgCons(parent, content) {
+    let contentArr = []
+    const allContent = parent.getElementsByTagName("div")
+    for (let i = 0; i < allContent.length; i++) {
         if (allContent[i].className == content) {
             contentArr.push(allContent[i])
         }
     }
-    // console.log(contentArr);
-    return contentArr
-}
-//获取列最高度最小列的位置下标函数
-function getMinHeightLocation(BoxHeightArr, minHeight) {
+    return contentArr;
+};
+
+//获取高度最小列的索引
+function getMinHeightColIndex(BoxHeightArr, minHeight) {
     for (var i in BoxHeightArr) {
         if (BoxHeightArr[i] === minHeight) {
-            return i
+            return i;
         }
     }
+}
+
+
+function checkImgOn(elem) {
+    var elems = document.getElementsByClassName('tempImg');
+    if (elems.length < 1) {
+        var imgElem = elem.getElementsByTagName("img");
+        var img = document.createElement('img');
+        body.appendChild(img);
+        img.setAttribute('class', 'tempImg');
+        img.setAttribute('src', imgElem[0].currentSrc);
+        img.setAttribute('onclick', 'checkImgOff(this)');
+        img.style.position = 'absolute';
+        img.style.width = '90%';
+        img.style.height = 'auto';
+        img.style.top = naviHeight + 'px';
+        img.style.left = ((container.offsetWidth - img.offsetWidth) / container.offsetWidth) * 50 + '%';
+        img.style.zIndex = '300';
+
+        container.style.opacity = 0.35;
+    }
+}
+
+function checkImgOff(elem) {
+    // var elem = document.getElementsByClassName('tempImg');
+    container.style.opacity = 1;
+    body.removeChild(elem);
 }
---
+++ script/resourceInfo.js
@@ -0,0 +1,86 @@
+const imgs =[
+    {
+        url: "./resource/image/IMG_0275.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0277.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0278.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/小物件书床头蓝.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/小米和华为背面.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/索尼耳机里面.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0279.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0280.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0281.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0282.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0284.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0285.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0286.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0287.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0288.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0289.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0290.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0291.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0292.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0293.JPG",
+        desc: ""
+    },
+    {
+        url: "./resource/image/IMG_0294.JPG",
+        desc: ""
+    },
+]
